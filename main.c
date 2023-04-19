c0f4c1d69cfbbb585bf93f53af168b7a6f8c221b
Gary Ho <garyho0916@gmail.com>
2023-04-19 00:47:23 -0700

    Fix addr cast error

--- main.c
+++ main.c
@@ -1,56 +1,53 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-int main(int argc, char *argv[])
-{
-    struct addrinfo hints, *res, *p;
-    int status;
+int main(int argc, char *argv[]) {
+  struct addrinfo hints, *res, *p;
+  int status;
 
-    // defined in netinet/in.h
-    char ipstr[INET6_ADDRSTRLEN];
+  // defined in netinet/in.h
+  char ipstr[INET6_ADDRSTRLEN];
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Error: showip hostname\n");
-        exit(1);
+  if (argc != 2) {
+    fprintf(stderr, "Error: showip hostname\n");
+    exit(1);
+  }
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+    fprintf(stderr, "[ERROR]: %s\n", gai_strerror(status));
+    return 2;
+  }
+
+  fprintf(stdout, "[LOG]  : IP addresses for %s:\n\n", argv[1]);
+
+  for (p = res; p != NULL; p = p->ai_next) {
+    void *addr;
+    char *ipver;
+    if (p->ai_family == AF_INET) { // IPv4
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+      addr = &(ipv4->sin_addr);
+      ipver = "IPv4";
+    } else { // IPv6
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+      addr = &(ipv6->sin6_addr);
+      ipver = "IPv6";
     }
+    // convert the IP to a string and print it:
+    inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+    printf(" %s: %s\n", ipver, ipstr);
+  }
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+  freeaddrinfo(res); // free the linked list
 
-    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
-    {
-        fprintf(stderr, "[ERROR]: %s\n", gai_strerror(status));
-        return 2;
-    }
-
-    fprintf(stdout, "[LOG]  : IP addresses for %s:\n\n", argv[1]);
-
-    for (p = res; p != NULL; p = p->ai_next)
-    {
-        void *addr;
-        char *ipver;
-        if (p->ai_addr->sa_family == AF_INET)
-        {
-            ipver = "IPv4";
-        }
-        else
-        {
-            ipver = "IPv6";
-        }
-        inet_ntop(p->ai_family, &(p->ai_addr), ipstr, sizeof ipstr);
-
-        fprintf(stdout, " %s: %s\n", ipver, ipstr);
-    }
-
-    freeaddrinfo(res); // free the linked list
-
-    return 0;
+  return 0;
 }
