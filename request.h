e40fbf0db0c3984a89bf6f22e2d0e79086d5e9dc
Gary Ho <garyho0916@gmail.com>
2023-05-05 13:33:20 -0700

    Add response type

--- request.h
+++ request.h
@@ -3,26 +3,25 @@
 
 #define HTTP_PROTOCOL "HTTP/1.1"
 
-#define DELEMITER " \t\r\n"
+#define DELEMITER " \r\n"
 #define KEY_VALUE_DELEMITER ":" DELEMITER
 
-#define ROUTE(req, url, met)                 \
+#define ROUTE(req, url, met)                                  \
     if (strncmp(req->header->route, url, strlen(url)) == 0 && \
-        strncmp(req->header->method, met, strlen(met)) == 0) 
+        strncmp(req->header->method, met, strlen(met)) == 0)
 
 #define GET(req, url) ROUTE(req, url, "GET")
 #define POST(req, url) ROUTE(req, url, "POST")
 #define PUT(req, url) ROUTE(req, url, "PUT")
 #define DELETE(req, url) ROUTE(req, url, "DELETE")
 
-// generate the response string
-#define RESPONSE(code, body)
-
 typedef struct header {
     char *method,   // GET, POST, PUT, DELETE
         *protocol,  // HTTP/1.1
         *route,     // /index.html
-        *host;      // localhost:8080
+        *host,      // localhost:8080
+        *msg;       // OK, Not Found, Internal Server Error
+    int code;       // 200, 404, 500
 } header_t;
 
 typedef struct request {
@@ -30,24 +29,56 @@ typedef struct request {
     char *body;
 } request_t;
 
+typedef struct response {
+    struct header *header;
+    char *body;
+} response_t;
+
 /**
  * @brief generate request
- * @return request
+ * @return request_t
  */
-request_t *generate_request();
+request_t *generate_request_t();
+
+/**
+ * @brief generate response
+ * @return response_t
+ */
+response_t *generate_response_t();
 
 /**
  * @brief parse request
+ * @param buf request string
  * @return request
  */
 request_t *parse_request(char *buf);
 
-char *generate_response(int code, char *body);
+/**
+ * @brief get full response
+ * @param res response_t
+ * @param code status code
+ * @param msg status message
+ * @param body response body
+ */
+void generate_response(response_t *res, int code, char *msg, char *body);
 
 /**
- * @brief free request
+ * @brief convert response to string
+ * @param res response string
+ * @param response_t
+ */
+void response_to_string(char *res_str, response_t *res);
+
+/**
+ * @brief free request_t
  * @param request
  */
-void free_request(request_t *request);
+void free_request_t(request_t *request);
+
+/**
+ * @brief free response_t
+ * @param response_t
+ */
+void free_response_t(response_t *response);
 
 #endif  // REQUE_H
