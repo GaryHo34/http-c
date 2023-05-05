25d6e82c289ddc27d82c954d427f201444bd9326
Gary Ho <garyho0916@gmail.com>
2023-05-05 13:33:48 -0700

    Wrap log with block

--- log.h
+++ log.h
@@ -1,15 +1,20 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <errno.h>
+#include <stdio.h>
+
 #define LOG_MSG(fmt, ...) fprintf(stdout, fmt "\n", ##__VA_ARGS__)
 
-#define ERROR_MSG(fmt, ...)                  \
-    char _buf[256];                          \
-    strerror_r(errno, _buf, sizeof(_buf));   \
-    if (!errno)                              \
-        fprintf(stderr, fmt "\n", ##__VA_ARGS__); \
-    else                                     \
-        fprintf(stderr, fmt ": %s\n", ##__VA_ARGS__, _buf)
+#define ERROR_MSG(fmt, ...)                                     \
+    do {                                                        \
+        char _buf[256];                                         \
+        strerror_r(errno, _buf, sizeof(_buf));                  \
+        if (!errno)                                             \
+            fprintf(stderr, fmt "\n", ##__VA_ARGS__);           \
+        else                                                    \
+            fprintf(stderr, fmt ": %s\n", ##__VA_ARGS__, _buf); \
+    } while (0)
 
 #define PANIC(fmt, ...)            \
     ERROR_MSG(fmt, ##__VA_ARGS__); \
