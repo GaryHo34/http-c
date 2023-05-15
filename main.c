3c59536d2c06963ebe6e833aac4473719e02854f
Gary Ho <garyho0916@gmail.com>
2023-05-14 17:18:41 -0700

    Add flag

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
 
+#include <unistd.h>
+
 #include "log.h"
 #include "request.h"
 #include "server.h"
-
+#include "threadp.h"
 void custom_controller(request *req, response *res) {
+    // simulate a long request
+    usleep(50000);
+
     GET(req, "/") {
-        printf("This is a %s request from %s\n",
+        printf("This is a %s request to route %s\n",
                req->header->method, req->header->route);
         generate_response(res, 200, "OK", "<h1> Hello World! </h1>");
         return;
     }
 
     POST(req, "/hello") {
-        printf("This is a %s request from %s\n",
+        printf("This is a %s request to route %s\n",
                req->header->method, req->header->route);
         generate_response(res, 200, "OK", "Hello World!");
         return;
@@ -25,19 +30,25 @@ int main(int argc, char *argv[]) {
     // get host and port from command line
     char *hostname = DEFAULT_HOST;
     char *port = DEFAULT_PORT;
-
+    int thread_num = DEFAULT_THREAD_NUM;
     int sockfd;
 
-    if (argc == 2) {
-        hostname = argv[1];
-    } else if (argc == 3) {
-        hostname = argv[1];
-        port = argv[2];
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-hostname") == 0) {
+            hostname = argv[++i];
+        } else if (strcmp(argv[i], "-port") == 0) {
+            port = argv[++i];
+        } else if (strcmp(argv[i], "-thread") == 0) {
+            int num = atoi(argv[++i]);
+            if (num > 0 && num <= 32) {
+                thread_num = num;
+            }
+        }
     }
 
     init_server(&sockfd, hostname, port);
 
-    server_run(sockfd, custom_controller);
+    server_run(sockfd, custom_controller, thread_num);
 
     return 0;
 }
